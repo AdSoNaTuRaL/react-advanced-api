62f0a42d2bf30967d155c2793c7d1309941a1c14
adsonatural <adsonhenriquesilva@gmail.com>
2021-09-29 14:02:43 +0200

    First commmit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b7cc95187613b8dff0537f86cef96539'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4ac0b2a2-e8de-41f4-8572-c1d4afaba082'
+};
